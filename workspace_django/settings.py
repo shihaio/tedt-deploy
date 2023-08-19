868cf773969c9ed32a4817fd53d11d5c5a08d391
shihaio <shihao.mymail@gmail.com>
2023-08-19 23:24:33 +0800

    fix bug CORS

--- workspace_django/settings.py
+++ workspace_django/settings.py
@@ -91,8 +91,8 @@ AUTH_USER_MODEL = "workspace.User"
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000"
 # ]
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
